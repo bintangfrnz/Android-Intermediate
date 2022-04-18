76325bdffab937723fd4ba45b560dde635964147
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-04-18 15:35:39 +0700

    stackwidget exercise

---
+++ 1-AdvanceUI/StackWidget/app/src/main/java/com/bintangfajarianto/stackwidget/StackWidgetService.kt
@@ -0,0 +1,9 @@
+package com.bintangfajarianto.stackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
