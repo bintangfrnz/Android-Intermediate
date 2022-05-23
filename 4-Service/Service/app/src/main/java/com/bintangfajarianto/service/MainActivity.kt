9b971cd929442cce760500e6e7fa73c39036f7e0
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-05-23 09:33:32 +0700

    service exercise

---
+++ 4-Service/Service/app/src/main/java/com/bintangfajarianto/service/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bintangfajarianto.service
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+        val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+        }
+        val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {
+
+        }
+    }
+}
